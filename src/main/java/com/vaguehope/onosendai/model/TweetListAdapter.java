2218362cbe3c257789310a01dcfd4615230c8142
Peng Yen Chao <mars.90226@gmail.com>
2013-12-24 00:28:04 +0800

    Fix bug about sortByTimeline

--- src/main/java/com/vaguehope/onosendai/model/TweetListAdapter.java
+++ src/main/java/com/vaguehope/onosendai/model/TweetListAdapter.java
@@ -169,7 +169,7 @@ public class TweetListAdapter extends BaseAdapter {
 
     public void sortByTimeline() {
 	    ArrayList<Tweet> result = new ArrayList<Tweet>(listData.getTweets());
-        Collections.sort(result,  new Comparator<TweetTreeNode>() {
+        Collections.sort(result,  new Comparator<Tweet>() {
 	            @Override
 	            public int compare(Tweet o1, Tweet o2) {
 	                long t1 = o1.getTime();
